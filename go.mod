a11d1c4c688ac0852c6259c5381887483b225568
Mustafa Akçakaya <mustafa.akcakaya@trendyol.com>
2024-02-19 11:33:47 +0300

    add isSame method test cases

--- go.mod
+++ go.mod
@@ -7,16 +7,19 @@ require (
 	github.com/labstack/gommon v0.4.0
 	github.com/sergi/go-diff v1.3.1
 	github.com/spf13/cobra v1.7.0
+	github.com/stretchr/testify v1.7.0
 	github.com/valyala/fasthttp v1.50.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
--- internal/monitor_service_test.go
+++ internal/monitor_service_test.go
@@ -1,1 +1,116 @@
 package internal
+
+import (
+	"github.com/Trendyol/es-alert-cli/pkg/model"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_Different_Monitor_Should_Return_Changed(t *testing.T) {
+	// Arrange
+	localMonitor := createTestMonitor("monitor1", "destination1")
+	remoteMonitor := createTestMonitor("monitor1", "destination2")
+
+	// Act
+	changed := isMonitorChanged(localMonitor, remoteMonitor)
+
+	// Assert
+	assert.True(t, changed, "The monitors should be considered changed")
+}
+
+func Test_Same_Monitor_Should_Return_Same(t *testing.T) {
+	// Arrange
+	localMonitor := createTestMonitor("monitor1", "destination1")
+	remoteMonitor := createTestMonitor("monitor1", "destination1")
+
+	// Act
+	changed := isMonitorChanged(localMonitor, remoteMonitor)
+
+	// Assert
+	assert.False(t, changed, "The monitors should be considered changed")
+}
+
+func createTestMonitor(monitorName string, destinationName string) model.Monitor {
+	// Create test Schedule
+	schedule := model.Schedule{
+		Period: model.Period{
+			Interval: 5,
+			Unit:     "minutes",
+		},
+	}
+
+	input := createTestInput()
+
+	trigger := createTestTrigger(destinationName)
+
+	monitor := model.Monitor{
+		ID:       "monitor1",
+		Type:     "type1",
+		Name:     monitorName,
+		Enabled:  true,
+		Schedule: schedule,
+		Inputs: []model.Input{
+			input,
+		},
+		Triggers: []model.Trigger{
+			trigger,
+		},
+	}
+
+	return monitor
+}
+
+func createTestTrigger(destinationName string) model.Trigger {
+	return model.Trigger{
+		ID:       "trigger1",
+		Name:     "trigger1",
+		Severity: "high",
+		Condition: model.Condition{
+			Script: model.Script{
+				Source: "source code",
+				Lang:   "painless",
+			},
+		},
+		Actions: createTestActions(destinationName),
+	}
+}
+
+func createTestActions(destinationName string) []model.Action {
+	return []model.Action{
+		{
+			Name:          "action1",
+			DestinationID: destinationName,
+			SubjectTemplate: model.Script{
+				Source: "subject source",
+				Lang:   "painless",
+			},
+			MessageTemplate: model.Script{
+				Source: "message source",
+				Lang:   "painless",
+			},
+		},
+	}
+}
+
+func createTestInput() model.Input {
+	return model.Input{
+		Search: model.Search{
+			Indices: []string{"index1", "index2"},
+			Query: model.QueryParam{
+				Query: model.InnerQuery{
+					Bool: model.BoolParam{
+						AdjustPureNegative: true,
+						Boost:              1.0,
+						Must: []model.MustParam{
+							{
+								Match: map[string]interface{}{
+									"field1": "value1",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
